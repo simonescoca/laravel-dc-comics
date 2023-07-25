24ad3828e064f73bc96c53d310ce38d9ca4f80b0
simonescoca <simone.scoca@icloud.com>
2023-07-25 17:03:09 +0200

    added func store to controller

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -27,7 +27,7 @@ class ComicController extends Controller
      */
     public function create()
     {
-        //
+        return view('create');
     }
 
     /**
@@ -38,7 +38,20 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request -> all();
+        $newComic = new Comic();
+        $newComic -> title = $data["title"];
+        $newComic -> description = $data["description"];
+        $newComic -> thumb = $data["thumb"];
+        $newComic -> price = $data["price"];
+        $newComic -> series = $data["series"];
+        $newComic -> sale_date = $data["sale_date"];
+        $newComic -> type = $data["type"];
+        $newComic -> artists = $data["artists"];
+        $newComic -> writers = $data["writers"];
+        $newComic -> save();
+
+        return redirect() -> route("show", $newComic -> id);
     }
 
     /**
